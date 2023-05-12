8024dbab98adac63e5955da0dc93b55d66ca6654
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-05-12 05:20:02 -0500

    renamed test file, adjusted test with correct ideal values

--- src/__test__/calculate-bmi.test.ts
+++ src/__test__/calculate-bmi.test.ts
@@ -10,7 +10,10 @@ describe('calculate-bmi for metric information', () => {
       kg: 80,
     };
     const bmi = calculateBMI(metricInformation);
-    expect(bmi).toBe(23.4);
+    expect(bmi.bmi).toBe("23.4");
+    expect(bmi.message).toBe(
+      "Your BMI suggests you're a healthy weight. Your ideal weight is between 63.3kgs - 85.2kgs."
+    );
   });
 
   test('height of 165, weight of 82, 30.1', () => {
@@ -20,31 +23,40 @@ describe('calculate-bmi for metric information', () => {
       kg: 82,
     };
     const bmi = calculateBMI(metricInformation);
-    expect(bmi).toBe(30.1);
+    expect(bmi.bmi).toBe("30.1");
+    expect(bmi.message).toBe(
+      "Your BMI suggets you're obese. Your ideal weight is between 50.4kgs - 67.8kgs."
+    );
   });
 });
 describe('calculate-bmi for imperial information', () => {
   test('height of 5ft 11, weight of 158, returns 22.0', () => {
     const imperialInformation: ImperialInfo = {
       type: 'imperial',
-      ft: 5,
-      in: 11,
+      feet: 5,
+      inches: 11,
       stone: 11,
       lb: 4,
     };
     const bmi = calculateBMI(imperialInformation);
-    expect(bmi).toBe(22.0);
+    expect(bmi.bmi).toBe("22.0");
+    expect(bmi.message).toBe(
+      "Your BMI suggests you're a healthy weight. Your ideal weight is between 9st 6lbs - 12st 10lbs."
+    );
   });
 
   test('height of 5ft 5, weight of 181, returns 30.1', () => {
     const imperialInformation: ImperialInfo = {
       type: 'imperial',
-      ft: 5,
-      in: 5,
+      feet: 5,
+      inches: 5,
       stone: 12,
       lb: 13,
     };
     const bmi = calculateBMI(imperialInformation);
-    expect(bmi).toBe(30.1);
+    expect(bmi.bmi).toBe("30.1");
+    expect(bmi.message).toBe(
+      "Your BMI suggets you're obese. Your ideal weight is between 7st 13lbs - 10st 9lbs."
+    );
   });
 });
