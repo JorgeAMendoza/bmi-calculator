155b5a42d0f9cb069f79b97320e9fa73eafd6c74
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-05-27 12:18:30 -0500

    create global styled with font applied, create general utility components

---
+++ src/styles/Utils.styled.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: min(90%, 116rem);
+  margin: 0 auto;
+`;
+
+export const MainHeading = styled.h1`
+  font-size: clamp(4.8rem, 5vw + 1rem, 6.4rem);
+  font-weight: 600;
+`;
+
+export const LargeHeading = styled.h2`
+  font-size: clamp(3.2rem, 5vw + 1rem, 4.8rem);
+  font-weight: 600;
+`;
+
+export const MediumHeading = styled.h3`
+  font-size: clamp(2.4rem, 5vw + 1rem, 3.2rem);
+  font-weight: 600;
+`;
+
+export const SmallHeading = styled.h4`
+  font-size: 2rem;
+  font-weight: 600;  
+`;
