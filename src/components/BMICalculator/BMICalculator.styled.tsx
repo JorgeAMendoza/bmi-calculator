37131e1629f2c76d83e06129c3a95fba70fc0787
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-05-29 11:15:39 -0500

    impored styled components into calculator component, create base structure style for the component

---
+++ src/components/BMICalculator/BMICalculator.styled.tsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+const BMICalculator = styled.form`
+  border-radius: 16px;
+  padding: 1.3em 1.4em;
+  text-align: left;
+
+  & > *:not(:last-child) {
+    border: 1px solid red;
+    margin-bottom: 2.3rem;
+  }
+`;
+
+const CalculatorTitle = styled.h2`
+  font-size: 2.4rem;
+  color: var(--gun-metal);
+  font-weight: 600;
+  letter-spacing: -0.05em;
+  word-spacing: -0.1em;
+`;
+
+const Form = styled.form``;
+
+const UnitSelect = styled.div`
+  display: flex;
+  border: 1px solid red;
+`;
+
+const FormInput = styled.div``;
+
+const InputArea = styled.div`
+  display: flex;
+
+  & > * {
+    flex-grow: 1;
+  }
+`;
+
+const Result = styled.div`
+  background: linear-gradient(90deg, #345ff7 0%, #587dff 100%);
+  padding: 2em;
+  color: var(--white);
+  border-radius: 16px;
+`;
+
+const BMIResult = styled.div`
+    
+`;
+
+const WelcomeMessage = styled.div`
+  p {
+    font-size: 2.4rem;
+    font-weight: 600;
+  }
+
+  p:last-of-type {
+    font-weight: 400;
+    font-size: 1.4rem;
+    letter-spacing: default;
+  }
+`;
+
+export default {
+  BMICalculator,
+  CalculatorTitle,
+  Form,
+  UnitSelect,
+  FormInput,
+  InputArea,
+  Result,
+  WelcomeMessage,
+  BMIResult
+};
--- src/components/BMICalculator/BMICalculator.tsx
+++ src/components/BMICalculator/BMICalculator.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect } from 'react';
 import Input from '../HookForm/Input';
 import BMIReducer, { CalculatorState } from '../../reducer/bmi-reducer';
 import RadioInput from '../HookForm/RadioInput';
+import Styled from './BMICalculator.styled';
 
 const initialState: CalculatorState = {
   unit: 'metric',
@@ -22,88 +23,104 @@ const BMICalculator = () => {
     dispatch({ type: 'CALCULATE_BMI' });
   }, [cm, kg, feet, inches, lb, stone]);
   return (
-    <div>
-      <p>Enter your details below</p>
-      <form>
+    <Styled.BMICalculator>
+      <Styled.CalculatorTitle>Enter your details below</Styled.CalculatorTitle>
+
+      <Styled.UnitSelect>
+        <RadioInput
+          val={unit}
+          dispatch={dispatch}
+          action="SET_UNIT"
+          name="metric"
+          testId="metricSelect"
+        />
+        <RadioInput
+          val={unit}
+          dispatch={dispatch}
+          action="SET_UNIT"
+          name="imperial"
+          testId="imperialSelect"
+        />
+      </Styled.UnitSelect>
+
+      {/* for metric input */}
+      {unit === 'metric' ? (
         <div>
-          <RadioInput
-            val={unit}
-            dispatch={dispatch}
-            action="SET_UNIT"
-            name="metric"
-            testId="metricSelect"
-          />
-          <RadioInput
-            val={unit}
-            dispatch={dispatch}
-            action="SET_UNIT"
-            name="imperial"
-            testId="imperialSelect"
-          />
-        </div>
-
-        {/* for metric input */}
-        {unit === 'metric' ? (
+          {/* height */}
           <div>
-            <p>metric form</p>
-            {/* height */}
             <p>height</p>
-            <Input
-              val={cm}
-              dispatch={dispatch}
-              action="SET_CM"
-              testId="cmInput"
-              name="cm"
-            />
-            {/* weight */}
-            <p>weight</p>
-            <Input
-              val={kg}
-              dispatch={dispatch}
-              action="SET_KG"
-              testId="kgInput"
-              name="kg"
-            />
+            <div>
+              <Input
+                val={cm}
+                dispatch={dispatch}
+                action="SET_CM"
+                testId="cmInput"
+                name="cm"
+              />
+            </div>
           </div>
-        ) : (
+
+          {/* weight */}
           <div>
-            <p>imperial form</p>
-            {/* height */}
-            <Input
-              val={feet}
-              dispatch={dispatch}
-              action="SET_FEET"
-              name="ft"
-              testId="ftInput"
-            />
-            <Input
-              val={inches}
-              dispatch={dispatch}
-              action="SET_INCHES"
-              testId="inInput"
-              name="in"
-            />
-
-            {/* weight */}
-
-            <Input
-              val={stone}
-              dispatch={dispatch}
-              action="SET_STONE"
-              testId="stInput"
-              name="st"
-            />
-            <Input
-              val={lb}
-              dispatch={dispatch}
-              action="SET_LB"
-              testId="lbInput"
-              name="lb"
-            />
+            <p>weight</p>
+            <div>
+              <Input
+                val={kg}
+                dispatch={dispatch}
+                action="SET_KG"
+                testId="kgInput"
+                name="kg"
+              />
+            </div>
           </div>
-        )}
-      </form>
-      <div aria-live="polite">
+        </div>
+      ) : (
+        <div>
+          {/* height */}
+          <div>
+            <p>height</p>
+            <Styled.InputArea>
+              <Input
+                val={feet}
+                dispatch={dispatch}
+                action="SET_FEET"
+                name="ft"
+                testId="ftInput"
+              />
+              <Input
+                val={inches}
+                dispatch={dispatch}
+                action="SET_INCHES"
+                testId="inInput"
+                name="in"
+              />
+            </Styled.InputArea>
+          </div>
+
+          {/* weight */}
+          <div>
+            <p>weight</p>
+            <Styled.InputArea>
+              <Input
+                val={stone}
+                dispatch={dispatch}
+                action="SET_STONE"
+                testId="stInput"
+                name="st"
+              />
+              <Input
+                val={lb}
+                dispatch={dispatch}
+                action="SET_LB"
+                testId="lbInput"
+                name="lb"
+              />
+            </Styled.InputArea>
+          </div>
+        </div>
+      )}
+
+      <Styled.Result aria-live="polite">
         {bmiInfo !== null ? (
           <>
             <p>
@@ -113,16 +130,16 @@ const BMICalculator = () => {
             <p data-cy="bmiResultText">{bmiInfo.message}</p>
           </>
         ) : (
-          <>
+          <Styled.WelcomeMessage>
             <p>Welcome</p>
             <p>
               Enter your height and weight and you&#39;ll see your BMI result
               here
             </p>
-          </>
+          </Styled.WelcomeMessage>
         )}
-      </div>
-    </div>
+      </Styled.Result>
+    </Styled.BMICalculator>
   );
 };
 
