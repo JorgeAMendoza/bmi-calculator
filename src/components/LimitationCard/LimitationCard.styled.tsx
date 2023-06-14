26eb7d6a4cc847282daabab10c16de8a67a18342
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-06-13 19:05:02 -0500

    adjust font size for small heading, adjust spacing between icon and card title

--- src/components/LimitationCard/LimitationCard.styled.tsx
+++ src/components/LimitationCard/LimitationCard.styled.tsx
@@ -1,15 +1,20 @@
 import styled from 'styled-components';
+import { SmallHeading } from '../../styles/Utils.styled';
 
 const LimitationCard = styled.div`
   filter: drop-shadow(16px 32px 56px rgba(143, 174, 207, 0.25));
   background-color: #fff;
   border-radius: 16px;
   padding: 1.5em;
+
+  & ${SmallHeading} {
+    letter-spacing: -0.05em;
+  }
 `;
 
 const Title = styled.div`
   display: flex;
-  gap: 0.8rem;
+  gap: 1.2rem;
   align-items: center;
   margin-block-end: 1.5rem;
 `;
